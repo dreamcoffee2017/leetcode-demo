3f5fdcffab3bd6e64156658fe2ce6d3b81e2e501
Frank_Chen <frank_chen@epam.com>
2020-03-20 17:30:46 +0800

    update

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # 配置
-kakazhu/3
-tempFilePath=D:\project\leetcode-demo\src\main\java
-codeFileName=$!velocityTool.camelCaseName(${question.titleSlug})Test
-codeTemplate=${question.content}
+* kakazhu/3
+* tempFilePath=D:\project\leetcode-demo\src\main\java
+* check custom code template
+* codeFileName=$!velocityTool.camelCaseName(${question.titleSlug})Test
+* codeTemplate=
+```
+${question.content}
 
 package leetcode.editor.cn;
 
 public class $!velocityTool.camelCaseName(${question.titleSlug})Test {
 
+    public static void main(String[] args) {
+        System.out.println(new $!velocityTool.camelCaseName(${question.titleSlug})Test().new Solution());
+    }
+
 ${question.code}
 }
+```
--- src/main/java/leetcode/editor/cn/test/Test.java
+++ src/main/java/leetcode/editor/cn/test/Test.java
@@ -1,6 +1,6 @@
 package leetcode.editor.cn.test;
 
-import java.io.*;
+import java.util.Arrays;
 
 /**
  * Test
@@ -8,65 +8,109 @@ import java.io.*;
  * @author Administrator
  * @date 2019/12/25
  */
-public class Test<T> {
-    public static <T> void main(String[] args) throws Exception {
-        Teacher teacher = new Teacher();
-        teacher.setAge(23);
-        teacher.setName("niesong");
-    }
-}
-interface A<T>{
-    
-}
+public class Test {
+    static int c = 0;
 
-class Teacher implements Serializable {
-//	private static final long serialVersionUID = 8940196742313994740L;
-    private int age;
-    private String name;
-
-    public int getAge() {
-        return age;
+    public static void main(String[] args) {
+        int[] a = {1, 4, 5, 6, 7, 8, 10, 222};
+//        new Test()
+//                .bubbleSort(a);
+//                .mergeSort(a);
+//                .quickSort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
+        System.out.println(new Test().binarySearch(a, 222));
+        System.out.println(c);
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    void bubbleSort(int[] arr) {
+        for (int len = arr.length - 1; len > 0; len--) {
+            for (int i = 0; i < len; i++) {
+                c++;
+                if (arr[i] > arr[i + 1]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp;
+                }
+            }
+        }
     }
 
-    public String getName() {
-        return name;
+    void mergeSort(int[] arr) {
+        if (arr.length > 1) {
+            int mid = arr.length / 2;
+            int[] leftArr = Arrays.copyOf(arr, mid);
+            int[] rightArr = Arrays.copyOfRange(arr, mid, arr.length);
+            mergeSort(leftArr);
+            mergeSort(rightArr);
+            int i = 0, j = 0, k = 0;
+            while (i < leftArr.length && j < rightArr.length) {
+                c++;
+                if (leftArr[i] < rightArr[j]) {
+                    arr[k++] = leftArr[i++];
+                } else {
+                    arr[k++] = rightArr[j++];
+                }
+            }
+            while (i < leftArr.length) {
+                c++;
+                arr[k++] = leftArr[i++];
+            }
+            while (j < rightArr.length) {
+                c++;
+                arr[k++] = rightArr[j++];
+            }
+        }
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-}
-
-class Student implements Serializable {
-    private int age;
-    private String name;
-    private Teacher teacher;
-
-    public int getAge() {
-        return age;
+    void quickSort(int[] arr, int first, int last) {
+        if (first < last) {
+            int pivotIdx = partition(arr, first, last);
+            quickSort(arr, first, pivotIdx - 1);
+            quickSort(arr, pivotIdx + 1, last);
+        }
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    private int partition(int[] arr, int first, int last) {
+        int pivot = arr[first];
+        int lower = first + 1;
+        int upper = last;
+        boolean done = false;
+        while (!done) {
+            while (lower <= upper && pivot >= arr[lower]) {
+                c++;
+                lower++;
+            }
+            while (lower <= upper && pivot <= arr[upper]) {
+                c++;
+                upper--;
+            }
+            if (lower > upper) {
+                done = true;
+            } else {
+                int tmp = arr[lower];
+                arr[lower] = arr[upper];
+                arr[upper] = tmp;
+            }
+        }
+        arr[first] = arr[upper];
+        arr[upper] = pivot;
+        return upper;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Teacher getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(Teacher teacher) {
-        this.teacher = teacher;
+    Integer binarySearch(int[] arr, int item) {
+        int lower = 0, upper = arr.length - 1;
+        while (lower <= upper) {
+            c++;
+            int mid = (lower + upper) / 2;
+            if (arr[mid] == item) {
+                return mid;
+            }
+            if (item < arr[mid]) {
+                upper = mid - 1;
+            } else {
+                lower = mid + 1;
+            }
+        }
+        return null;
     }
 }
